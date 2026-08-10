b5158c22a2072fa3dc4dfbe6f4bae5bc6786c1be
Vengatesh Narayanan <270347663+VengateshNarayanan@users.noreply.github.com>
2026-08-10 14:41:07 +0000

    Basic Input Ouput I/O

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS && repositoryName ? `/${repositoryName}/` : '/',
-  plugins: [
-    tailwindcss(),
-  ],
+    base: process.env.GITHUB_ACTIONS && repositoryName
+        ? `/${repositoryName}/`
+        : '/',
+    plugins: [
+        tailwindcss(),
+    ],
 })
